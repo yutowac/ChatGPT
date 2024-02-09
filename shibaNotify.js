e5c56a90ad7ebd561e2939b77ef7b24e9ae509ff
yuto <44987057+yutowac@users.noreply.github.com>
2024-02-09 23:55:57 +0900

    Update shibaNotify.js

--- shibaNotify.js
+++ shibaNotify.js
@@ -21,7 +21,6 @@ async function fetchRandomShibeImage() {
         console.error('Error fetching shibe image:', error);
     }
 }
-
 // LINE Notifyに画像を送信する関数
 const sendNotify = async (message, imageUrl, access_token) => {
     const url = 'https://notify-api.line.me/api/notify';
